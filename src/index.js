6a5ec05bd7ed6aae6f8d38085c109e6cf58b44d8
Andrey Nikolaev <andryuha_nikolaev@mail.ru>
2023-03-28 18:29:45 +0300

    feat: класс попапа, кнопка регистрации

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import './index.html';
 
 import './js/modules/slider';
-import './js/modules/burger';
 
 import './scss/index.scss';
+
+import './js/modules/burger';
+import Popup from './js/components/Popup';
 
 // кнопка войти
 const loginBtn = document.getElementById('login-btn');
@@ -16,43 +18,50 @@ const closeBtn = document.querySelector('.popup__close-button');
 // оверлей попапа
 const overlay = document.querySelector('.popup__overlay');
 
-//функция открытия попапа
-const openPopup = () => {
-  // closeBurger();
-  popup.classList.add('popup_opened'); //добавляем класс открытого попапа
-  document.addEventListener('keydown', handleEscClose); //ставим слушатель на нажатие esc
-};
+// //функция открытия попапа
+// const openPopup = () => {
+//   // closeBurger();
+//   popup.classList.add('popup_opened'); //добавляем класс открытого попапа
+//   document.addEventListener('keydown', handleEscClose); //ставим слушатель на нажатие esc
+// };
 
-//функция закрытия попапа
-const closePopup = () => {
-  popup.classList.remove('popup_opened'); //убираем класс открытого попапа
-  document.removeEventListener('keydown', handleEscClose); //убираем слушатель на нажатие esc
-};
+// //функция закрытия попапа
+// const closePopup = () => {
+//   popup.classList.remove('popup_opened'); //убираем класс открытого попапа
+//   document.removeEventListener('keydown', handleEscClose); //убираем слушатель на нажатие esc
+// };
 
-//функция закрытия попапов на esc
-const handleEscClose = (evt) => {
-  if (evt.key === 'Escape') {
-    //если действие происходит на кнопке esc
-    closePopup(); //то вызвать функцию закрытия попапа
-  }
-};
+// //функция закрытия попапов на esc
+// const handleEscClose = (evt) => {
+//   if (evt.key === 'Escape') {
+//     //если действие происходит на кнопке esc
+//     closePopup(); //то вызвать функцию закрытия попапа
+//   }
+// };
 
-// слушатель клика по кнопке войти
-loginBtn.addEventListener('click', () => {
-  openPopup();
-});
+// // слушатель клика по кнопке войти
+// loginBtn.addEventListener('click', () => {
+//   openPopup();
+// });
 
-// слушатель клика по кнопке зарегистрироваться
+// // слушатель клика по кнопке зарегистрироваться
+// registerBtn.addEventListener('click', () => {
+//   openPopup();
+// });
+
+// // слушатель клика по кнопке закрытия попапа
+// closeBtn.addEventListener('click', () => {
+//   closePopup();
+// });
+
+// // слушатель клика по оверлею
+// overlay.addEventListener('click', () => {
+//   closePopup();
+// });
+
 registerBtn.addEventListener('click', () => {
-  openPopup();
+  popupRegister.open(); // открываем попап
 });
 
-// слушатель клика по кнопке закрытия попапа
-closeBtn.addEventListener('click', () => {
-  closePopup();
-});
-
-// слушатель клика по оверлею
-overlay.addEventListener('click', () => {
-  closePopup();
-});
+const popupRegister = new Popup('#popup-register');
+popupRegister.setEventListeners(); // навешиваем слушатели
---
+++ src/js/components/Popup.js
@@ -0,0 +1,37 @@
+export default class Popup {
+  constructor(popupSelector) {
+    this._popup = document.querySelector(popupSelector);
+    this._handleEscClose = this._handleEscClose.bind(this);
+  }
+
+  //функция открытия попапа
+  open() {
+    this._popup.classList.add('popup_opened'); //добавляем класс открытого попапа
+    document.addEventListener('keydown', this._handleEscClose); //ставим слушатель на нажатие esc
+  }
+
+  //функция закрытия попапа
+  close() {
+    this._popup.classList.remove('popup_opened'); //убираем класс открытого попапа
+    document.removeEventListener('keydown', this._handleEscClose); //убираем слушатель на нажатие esc
+  }
+
+  //функция закрытия попапов на esc
+  _handleEscClose(evt) {
+    if (evt.key === 'Escape') {
+      //если действие происходит на кнопке esc
+      this.close(); //то вызвать функцию закрытия попапа
+    }
+  }
+
+  //слушатель закрытия попапов
+  setEventListeners() {
+    this._popup.addEventListener('mousedown', (evt) => {
+      if (evt.target.classList.contains('popup__overlay')) {
+        this.close();
+      } else if (evt.target.classList.contains('popup__close-button')) {
+        this.close();
+      }
+    });
+  }
+}
